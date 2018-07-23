50a57ef5073e7e00b7419096755dc4ce4a381733
samsamfish5422 <samsamfish5422@gmail.com>
2018-07-23 23:05:41 +0800

    Test

---
+++ app/src/main/java/com/example/user/title/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.user.title;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import static java.security.AccessController.getContext;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       /**  ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            actionBar.hide();
+        }  **/
+
+        Button titleBack = (Button) findViewById(R.id.title_back);
+        Button titleEdit = (Button)findViewById(R.id.title_edit);
+        titleBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((Activity)getApplicationContext()).finish();
+            }
+        });
+
+        titleEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "You clicked Edit button", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/user/title/TitleLayout.java
@@ -0,0 +1,17 @@
+package com.example.user.title;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+
+public class TitleLayout extends LinearLayout {
+
+    public TitleLayout(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        LayoutInflater.from(context).inflate(R.layout.title, this);
+    }
+}
